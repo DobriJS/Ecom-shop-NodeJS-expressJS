d33deb93250082d70862e62ec5fb737dbf50ccea
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-11-09 20:53:07 +0200

    Install Mongoose and implement User and Product Schema

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use((req, res, next) => {
+  User.findById("5fa98f22ef992c3e80b933cf")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+  .connect(
+    "mongodb://DobriJS:slayer198900@eshop-shard-00-00.phkmi.mongodb.net:27017,eshop-shard-00-01.phkmi.mongodb.net:27017,eshop-shard-00-02.phkmi.mongodb.net:27017/Eshop?ssl=true&replicaSet=atlas-c8930y-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Dobri",
+          email: "max@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
